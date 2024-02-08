f3f1edad391ba2dc288eb1fc52474f8a79d4179c
lilman109 <akira.yoshidasjsu@gmail.com>
2024-02-08 23:08:06 +0900

    implemented settings page

--- src/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -51,7 +51,7 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
 
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
-          <>
+      <>
         <div className="flex items-center justify-between mb-2">
           <Skeleton className="h-10 w-[50%]" />
           <Skeleton className="h-10 w-10" />
