ff8f6081c64f48ef07cf8d7de60f059afb8b50f3
lilman109 <akira.yoshidasjsu@gmail.com>
2024-02-17 12:06:19 +0900

    implemented navbar create board handling

--- src/app/(platform)/(dashboard)/_components/navbar.tsx
+++ src/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 import { MobileSidebar } from "./mobileSidebar";
+import { FormPopover } from "@/components/form";
 
 export const NavBar = () => {
   return (
@@ -12,16 +13,20 @@ export const NavBar = () => {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
-        >
-          Create
-        </Button>
-        <Button size="sm" className="rounded-sm block md:hidden">
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button size="sm" className="rounded-sm block md:hidden">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
