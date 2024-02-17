f8cfb698e06e16be240cfc4432b77ec53aa27d5a
lilman109 <akira.yoshidasjsu@gmail.com>
2024-02-17 16:20:36 +0900

    implemented oragnizationid page metadata

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,18 @@
 import { OrgControl } from "@/src/app/(platform)/(dashboard)/organization/_components/index";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> => {
+  const { orgSlug } = auth();
+
+  return {
+    title: `${orgSlug}`,
+  };
+};
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
